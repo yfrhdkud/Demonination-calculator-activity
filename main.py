520e9126acb4b1691723f4e203facc500745d6a7
EJA <eraajaswal@gmail.com>
2025-06-18 17:27:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ root.title("Denomination Counter")
 root.configure(bg = "light blue")
 root.geometry("650x400")
 
-upload = Image.open("cash img.jpeg")
+upload = Image.open("cash_img.jpeg")
 
-upload = upload.resize(300, 300)
+upload = upload.resize((300, 300))
 image = ImageTk.PhotoImage(upload)
 label = Label(root,image=image, bg="light blue")
 label.place(x=180, y=20)
-label1 = label(root,
-               Text = "Hello User.Welcome to the Denomination Counter Application",
+label1 = Label(root,
+               text = "Hello User.Welcome to the Denomination Counter Application",
                bg = "light blue"
 )
 label1.place(relx=0.5, y=340 , anchor = CENTER)
@@ -24,7 +24,7 @@ def msg():
         "Alert", "Do you want to calculate the denomination count"
     )
 
-    if MsgBox == "Ok":
+    if MsgBox == "ok":
         topwon()
 
 button = Button(
@@ -40,15 +40,15 @@ button.place(x=260, y=360)
 def topwon():
     top = Toplevel()
     top.title("Denomination calculator")
-    top.config(bg="light grey")
+    top.configure(bg="light grey")
     top.geometry("600x350+50+50")
     label = Label(top, text="Enter the total aomunt", bg="light grey")
     entry = Entry(top)
     lbl = Label(top, text="Here are the numbers for the denominations", bg="light grey")
 
-    l1 = Label(top, Text="2000" , bg="light grey")
-    l2 = Label(top, Text="500" , bg="light grey")
-    l3 = Label(top, Text="100" , bg="light grey")
+    l1 = Label(top, text="2000" , bg="light grey")
+    l2 = Label(top, text="500" , bg="light grey")
+    l3 = Label(top, text="100" , bg="light grey")
 
     t1 = Entry(top)
     t2 = Entry(top)
@@ -76,18 +76,18 @@ def topwon():
             messagebox.showerror("Error", "Please enter a valid number.")
     btn = Button(top, text="Calculate", command=calculator, bg="brown", fg="white")
 
-    label.place(X=230, y=50)
+    label.place(x=230, y=50)
     entry.place(x=200, y=80)
     btn.place(x=240, y=120)
     lbl.place(x=140, y=170)
 
     l1.place(x=180, y=200)
-    l1.place(x=180, y=230)
-    l1.place(x=180, y=260)
+    l2.place(x=180, y=230)
+    l3.place(x=180, y=260)
 
     t1.place(x=270, y=200)
-    t1.place(x=270, y=230)
-    t1.place(x=270, y=260)
+    t2.place(x=270, y=230)
+    t3.place(x=270, y=260)
 
     top.mainloop()
 
